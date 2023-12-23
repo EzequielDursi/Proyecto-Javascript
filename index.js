4ef94906cbfaa73e2ae58e74754ce0dbf665eff1
Ezequiel Dursi <ezequieldursi@gmail.com>
2023-12-23 13:52:44 -0300

    arrays y objetos

--- index.js
+++ index.js
@@ -1,30 +1,77 @@
+const historial = []
+class operacion{
+    constructor(simbolo, numeroA, numeroB, resultado){
+
+        this.simbolo = simbolo;
+        this.numeroA = numeroA
+        this.numeroB = numeroB
+        this.resultado = resultado
+    }
+}
 
 function sumar() {
-    const numeroA = parseInt(prompt("Ingrese primer numero"));
-    const numeroB = parseInt(prompt("Ingrese el otro numero"));
-    const resultado = numeroA + numeroB;
-    alert(numeroA + " + " + numeroB + " = " + resultado);
+    let numeroA = parseInt(prompt("Ingrese primer numero"))
+    while (Number.isNaN (numeroA)) {
+        numeroA = parseInt(prompt("Ingrese primer numero"))
+    }  ; 
+    let numeroB = parseInt(prompt("Ingrese el otro numero"))
+    while (Number.isNaN (numeroB)){ 
+        numeroB = parseInt(prompt("Ingrese el otro numero"))
+    };
+    let resultado = numeroA + numeroB;
+    alert(numeroA + " + " + numeroB + " = " + resultado)
+
+    const operacion = new Operacion (" + ", numeroA, numeroB, resultado)
+
+    historial.push(operacion);
 }
 
 function restar() {
-    const numeroA = parseInt(prompt("Ingrese primer numero"));
-    const numeroB = parseInt(prompt("Ingrese el otro numero"));
-    const resultado = numeroA - numeroB;
-    alert(numeroA + " - " + numeroB + " = " + resultado);
+    let numeroA = parseInt(prompt("Ingrese primer numero"))
+    while (Number.isNaN (numeroA)) {
+        numeroA = parseInt(prompt("Ingrese primer numero"))
+    }  ;
+    let numeroB = parseInt(prompt("Ingrese el otro numero"))
+    while (Number.isNaN (numeroB)){ 
+        numeroB = parseInt(prompt("Ingrese el otro numero"))
+    };
+    let resultado = numeroA - numeroB;
+    alert(numeroA + " - " + numeroB + " = " + resultado)
+    const operacion = new Operacion (" - ", numeroA, numeroB, resultado)
+
+    historial.push(operacion);
 }
 
 function multiplicar() {
-    const numeroA = parseInt(prompt("Ingrese primer numero"));
-    const numeroB = parseInt(prompt("Ingrese el otro numero"));
-    const resultado = numeroA * numeroB;
-    alert(numeroA + " * " + numeroB + " = " + resultado);
+    let numeroA = parseInt(prompt("Ingrese primer numero"))
+    while (Number.isNaN (numeroA)) {
+        numeroA = parseInt(prompt("Ingrese primer numero"))
+    }  ;
+    let numeroB = parseInt(prompt("Ingrese el otro numero"))
+    while (Number.isNaN (numeroB)){ 
+        numeroB = parseInt(prompt("Ingrese el otro numero"))
+    };
+    let resultado = numeroA * numeroB;
+    alert(numeroA + " * " + numeroB + " = " + resultado)
+    const operacion = new Operacion (" * ", numeroA, numeroB, resultado)
+
+    historial.push(operacion);
 }
 
 function dividir() {
-    const numeroA = parseInt(prompt("Ingrese primer numero"));
-    const numeroB = parseInt(prompt("Ingrese el otro numero"));
-    const resultado = numeroA / numeroB;
-    alert(numeroA + " / " + numeroB + " = " + resultado);
+    let numeroA = parseInt(prompt("Ingrese primer numero"))
+    while (Number.isNaN (numeroA)) {
+        numeroA = parseInt(prompt("Ingrese primer numero"))
+    }  ;
+    let numeroB = parseInt(prompt("Ingrese el otro numero"))
+    while (Number.isNaN (numeroB)){ 
+        numeroB = parseInt(prompt("Ingrese el otro numero"))
+    };
+    let resultado = numeroA / numeroB;
+    alert(numeroA + " / " + numeroB + " = " + resultado)
+    const operacion = new Operacion (" / ", numeroA, numeroB, resultado)
+
+    historial.push(operacion);
 }
 
 function calcularPromedio() {
@@ -53,7 +100,22 @@ function calcularRaizCuadrada() {
     alert("La raíz cuadrada de " + numero + " es: " + resultado);
 }
 
-let opcion = parseInt(prompt("Elija una opción: \n 1-sumar \n 2-restar \n 3-multiplicar \n 4-dividir \n 5-calcular promedio \n 6-calcular potencia \n 7-calcular raíz cuadrada \n 8-salir"));
+function verHistorial() { 
+    const simbolo = prompt ("elija un simbolo para filtar historial : \n + sumas \n - restas \n * multiplicacion \n / division")
+
+    const filtrado = historial.filter((elemento)=> {
+
+        return operacion.simbolo == simbolo
+    })
+
+    let mensaje = "";
+    filtrado.forEach((operacion)=> {
+        mensaje = mensaje + operacion.numeroA + operacion.simbolo + operacion.numeroB + " = " + operacion.resultado
+    
+            })
+       alert(mensaje);
+     }
+let opcion = parseInt(prompt("Elija una opción: \n 1-sumar \n 2-restar \n 3-multiplicar \n 4-dividir \n 5-calcular promedio \n 6-calcular potencia \n 7-calcular raíz cuadrada \n 8-Ver Historial \n 9-salir"));
 
 while (opcion !== 8) {
     switch (opcion) {
@@ -78,12 +140,16 @@ while (opcion !== 8) {
         case 7:
             calcularRaizCuadrada();
             break;
+        case 8:
+            verHistorial()
+            break;
         default:
             alert("Opcion incorrecta");
             break;
     }
 
-    opcion = parseInt(prompt("Elija una opción: \n 1-sumar \n 2-restar \n 3-multiplicar \n 4-dividir \n 5-calcular promedio \n 6-calcular potencia \n 7-calcular raíz cuadrada \n 8-salir"));
+   let opcion = parseInt(prompt("Elija una opción: \n 1-sumar \n 2-restar \n 3-multiplicar \n 4-dividir \n 5-calcular promedio \n 6-calcular potencia \n 7-calcular raíz cuadrada\n 8-Ver Historial \n 9-salir"));
 }
 
-alert("Finalizado programa, enter para cerrarlo");
+alert("Finalizado programa, enter para cerrarlo")
+console.log(historial);
